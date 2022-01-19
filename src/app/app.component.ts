a5703e9bae94a0381ea142d1ce6bd8dafce2ad10
Santiago Rodriguez Arroyave <santiago.rodriguez.2639@eam.edu.co>
2022-01-18 22:35:24 -0500

    version_1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,188 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Taller_Uno';
+
+  a: number = 0;
+  b: number = 0;
+  c: number = 0;
+  d: number = 0;
+  e: number = 0;
+  f: number = 0;
+  g: number = 0;
+  h: number = 0;
+  i: number = 0;
+  j: number = 0;
+  ad: number = 0;
+  bd: number = 0;
+  cd: number = 0;
+  dd: number = 0;
+  ed: number = 0;
+  fd: number = 0;
+  gd: number = 0;
+  hd: number = 0;
+  id: number = 0;
+  jd: number = 0;
+
+  constructor() {
+  }
+
+
+  contarIronMan() {
+
+    this.a += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarCapAmerica() {
+
+    this.b += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarThor() {
+
+    this.c += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarSpiderMan() {
+
+    this.d += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarLoki() {
+
+    this.e += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarHulk() {
+
+    this.f += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarDoctorStrange() {
+
+    this.g += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarCapitanaMarvel() {
+
+    this.h += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarViudaNegra() {
+
+    this.i += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarPanteraNegra() {
+
+    this.j += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+
+  contarIronManDislike() {
+
+    this.ad += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarCapAmericaDislike() {
+
+    this.bd += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarThorDislike() {
+
+    this.cd += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarSpiderManDislike() {
+
+    this.dd += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarLokiDislike() {
+
+    this.ed += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarHulkDislike() {
+
+    this.fd += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarDoctorStrangeDislike() {
+
+    this.gd += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarCapitanaMarvelDislike() {
+
+    this.hd += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  contarViudaNegraDislike() {
+
+    this.id += 1;
+    console.log("se ejecutar contar()")
+
+    }
+
+  contarPanteraNegraDislike() {
+
+    this.jd += 1;
+    console.log("se ejecutar contar()")
+
+  }
+
+  reset() {
+    this.a = 0;
+
+  }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/app/componentes/inicio/inicio.component.ts
@@ -0,0 +1,22 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent  {
+
+  n: number = 0;
+
+  constructor() {}
+
+
+/*resultados(
+
+)*/
+
+
+
+
+}
